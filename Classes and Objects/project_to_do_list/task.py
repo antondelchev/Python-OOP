802a5c55c2d8dcd2bd14f2e7d1f47527a69d046b
antondelchev <dlchv.mail@gmail.com>
2021-08-17 14:53:04 +0300

    Finished task.

--- Classes and Objects/project_to_do_list/task.py
+++ Classes and Objects/project_to_do_list/task.py
@@ -2,22 +2,34 @@ class Task:
     comments = []
     completed = False
 
-    def __init__(self, date, due_date):
-        self.date = date
+    def __init__(self, name, due_date):
+        self.name = name
         self.due_date = due_date
         self.tasks = []
 
     def change_name(self, new_name: str):
-        pass
+        if self.name == new_name:
+            return f"Name cannot be the same."
+        else:
+            self.name = new_name
+            return self.name
 
     def change_due_date(self, new_date: str):
-        pass
+        if self.due_date == new_date:
+            return f"Date cannot be the same."
+        else:
+            self.due_date = new_date
+            return self.due_date
 
     def add_comment(self, comment: str):
-        pass
+        Task.comments.append(comment)
 
     def edit_comment(self, comment_number: int, new_comment: str):
-        pass
+        if 0 <= comment_number < len(Task.comments):
+            Task.comments[comment_number] = new_comment
+            return ", ".join(Task.comments)
+        else:
+            return "Cannot find comment."
 
     def details(self):
-        pass
+        return f"Name: {self.name} - Due Date: {self.due_date}"
