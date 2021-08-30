cabd6f58c76462e6596a4bf7fc9e892dcc727005
antondelchev <dlchv.mail@gmail.com>
2021-08-30 13:17:03 +0300

    Update.

---
+++ Inheritance/project_players_and_monsters/elf.py
@@ -0,0 +1,12 @@
+from project_players_and_monsters.hero import Hero
+
+
+class Elf(Hero):
+    pass
+
+
+elf = Elf("E", 4)
+print(str(elf))
+print(elf.__class__.__bases__[0].__name__)
+print(elf.username)
+print(elf.level)
