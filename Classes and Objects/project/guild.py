e6bad00e50cde5263db96781062045fad6873990
antondelchev <dlchv.mail@gmail.com>
2021-10-30 21:18:35 +0300

    Update.

--- Classes and Objects/project/guild.py
+++ Classes and Objects/project/guild.py
@@ -7,31 +7,25 @@ class Guild:
         self.players = []
 
     def assign_player(self, player: Player):
-        if player in self.players:
-            if player.guild == self.name:
-                return f"Player {player.name} is already in the guild."
-            else:
-                return f"Player {player.name} is in another guild."
-        else:
-            self.players.append(player)
-            player.guild = self.name
-            return f"Welcome player {player.name} to the guild {self.name}"
+        if player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
 
-    def kick_player(self, player_name: str):
-        if player_name in self.players:
-            player_name.guild = "Unaffiliated"
-            return f"Player {player_name} has been removed from the guild."
-        else:
-            return f"Player {player_name} is not in the guild."
+        if not player.guild == self.name and not player.guild == "Unaffiliated":
+            return f"Player {player.name} is in another guild."
+
+        self.players.append(player)
+        player.guild = self.name
+        return f"Welcome player {player.name} to the guild {self.name}"
+
+    def kick_player(self, player_name):
+        for player in self.players:
+            if player.name == player_name:
+                self.players.remove(player)
+                player.guild = "Unaffiliated"
+                return f"Player {player_name} has been removed from the guild."
+
+        return f"Player {player_name} is not in the guild."
 
     def guild_info(self):
         result = [el.player_info() for el in self.players]
         return f"Guild: {self.name}\n" + "\n".join(result)
-
-
-player_one = Player("George", 50, 100)
-print(player_one.add_skill("Shield Break", 20))
-print(player_one.player_info())
-guild = Guild("UGT")
-print(guild.assign_player(player_one))
-print(guild.guild_info())
