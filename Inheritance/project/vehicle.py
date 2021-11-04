670ed2522e3d0347af2e17009d2514cde6311de7
antondelchev <dlchv.mail@gmail.com>
2021-11-04 22:55:54 +0200

    Update.

--- Inheritance/project/vehicle.py
+++ Inheritance/project/vehicle.py
@@ -1,10 +1,10 @@
 class Vehicle:
     DEFAULT_FUEL_CONSUMPTION = 1.25
 
-    def __init__(self, fuel, horse_power, fuel_consumption=DEFAULT_FUEL_CONSUMPTION):
+    def __init__(self, fuel, horse_power):
         self.fuel = fuel
         self.horse_power = horse_power
-        self.fuel_consumption = fuel_consumption
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
 
     def drive(self, kilometers):
         if self.fuel >= self.fuel_consumption * kilometers:
